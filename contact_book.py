5c5b7a1b4ad6d45e73f674037bdffc2c4c62dc99
Dunjasha <danylsavenkvo@gmai.com>
2025-04-18 20:45:59 +0200

    enbret2

--- contact_book.py
+++ contact_book.py
@@ -31,6 +31,7 @@ class Record:
         for phone in self.phones:
             if phone.value == old_phone:
                 phone.value = Phone(new_phone).value
+                return
         raise ValueError(f"Phone number '{old_phone}' not found.")
     
     def find_phone(self, phone_number):
@@ -43,6 +44,7 @@ class Record:
         for phone in self.phones:
             if phone.value == phone_number:
                 self.phones.remove(phone)
+                return
         raise ValueError(f"Phone number '{phone_number}' not found.")
     
     def __str__(self):
@@ -68,3 +70,38 @@ class AddressBook(UserDict):
             result += str(record) + "\n"
         return result.strip()
     
+
+
+
+
+# Створення нової адресної книги
+book = AddressBook()
+
+    # Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+# Додавання запису John до адресної книги
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+# Виведення всіх записів у книзі
+     
+print(book)
+
+# Знаходження та редагування телефону для John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
+# Видалення запису Jane
+book.delete("Jane")
