568b132d7645bb84d98861e1e300bccadc210ece
Dunjasha <danylsavenkvo@gmai.com>
2025-04-21 12:41:53 +0200

    en gang til

--- contact_book.py
+++ contact_book.py
@@ -24,15 +24,20 @@ class Record:
         self.phones = []
 
     def add_phone(self, phone):
+        if self.find_phone(phone):
+            raise ValueError(f"Phone number '{phone}' already exists.")
         self.phones.append(Phone(phone))
 
 
+
     def edit_phone(self, old_phone, new_phone):
-        for phone in self.phones:
-            if phone.value == old_phone:
-                phone.value = Phone(new_phone).value
-                return
-        raise ValueError(f"Phone number '{old_phone}' not found.")
+        if self.find_phone(new_phone):
+            raise ValueError(f"Phone number '{new_phone}' already exists.")
+        self.remove_phone(old_phone)
+        self.add_phone(new_phone)
+
+
+
     
     def find_phone(self, phone_number):
         for phone in self.phones:
@@ -41,11 +46,10 @@ class Record:
         return None
     
     def remove_phone(self, phone_number):
-        for phone in self.phones:
-            if phone.value == phone_number:
-                self.phones.remove(phone)
-                return
-        raise ValueError(f"Phone number '{phone_number}' not found.")
+        phone = self.find_phone(phone_number)
+        if not phone:
+            raise ValueError(f"Phone number '{phone_number}' not found.")
+        self.phones.remove(phone)
     
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
@@ -70,38 +74,3 @@ class AddressBook(UserDict):
             result += str(record) + "\n"
         return result.strip()
     
-
-
-
-
-# Створення нової адресної книги
-book = AddressBook()
-
-    # Створення запису для John
-john_record = Record("John")
-john_record.add_phone("1234567890")
-john_record.add_phone("5555555555")
-
-# Додавання запису John до адресної книги
-book.add_record(john_record)
-
-# Створення та додавання нового запису для Jane
-jane_record = Record("Jane")
-jane_record.add_phone("9876543210")
-book.add_record(jane_record)
-
-# Виведення всіх записів у книзі
-     
-print(book)
-
-# Знаходження та редагування телефону для John
-john = book.find("John")
-john.edit_phone("1234567890", "1112223333")
-
-print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
-
-# Пошук конкретного телефону у записі John
-found_phone = john.find_phone("5555555555")
-print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
-# Видалення запису Jane
-book.delete("Jane")
